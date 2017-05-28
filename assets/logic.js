59a06a9b83b921a41aeb29cba6ec9def71fff2e7
Raquel Rodriguez <raquelclare@Raquels-MacBook-Air.local>
2017-05-27 20:49:46 -0500

    adding time

--- assets/logic.js
+++ assets/logic.js
@@ -1,4 +1,4 @@
-//console.log("hello");
+console.log("hello");
 
 // Initializing Database
 $(document).ready(function() {
@@ -16,33 +16,45 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 
-$("#submitBtn").on("click", function() {
+$("#submitBtn").on("click", function(event) {
 
     event.preventDefault();
     // Creating variables to grab the input from the form
     var trainName 	= $("#train-name").val().trim();
-
     var destination = $("#destination").val().trim();
-    // This is actually a time so I might need to change this
     var firstTrain 	= $("#first-train").val().trim();
-    // This is also a time
     var frequency 	= $("#frequency").val().trim();
+
+    // Converting the time input into moment.js and pushing back 1 year to make sure it comes before current time
+    var timeConverted = moment(firstTrain, "hh:mm").subtract(1, "years");
+    console.log(timeConverted);
+    // Current time
+    var currentTime = moment();
+    // Getting the difference between current time and the timeConverted
+    var diffTime = currentTime.diff(moment(timeConverted), "minutes");
+    console.log(diffTime);
+    // Time apart (remainder)
+    var remainder = diffTime % frequency;
+    console.log(remainder);
+    // Minutes left until next train
+   	var minTillTrain = frequency - remainder;
+   	// Next train arrival time
+   	var nextTrain = currentTime.add(minTillTrain, "minutes");
+   	nextTrain = moment(nextTrain).format("hh:mm");
 
     var newTrain = {
         trainName: trainName,
         destination: destination,
         firstTrain: firstTrain,
-        frequency: frequency
+        frequency: frequency,
+        minTillTrain: minTillTrain,
+        nextTrain: nextTrain
     };
 
     database.ref().push(newTrain);
 });
 // Getting the references from the firebase database and creating "snapshots"
 database.ref().on("child_added", function(snapshot) {
-	// Function to calculate time for next arrival and has to dynamically update on the page
-	var nextArrival = "";
-	// Function to calculate how many minutes away the next train is and has to dynamically update on the page
-	var minAway = "";
 	// Putting all variables into one variable called fields
 	var fields = snapshot.val();
 
@@ -51,14 +63,14 @@ database.ref().on("child_added", function(snapshot) {
 	var destination = fields.destination;
 	var firstTrain 	= fields.firstTrain;
 	var frequency 	= fields.frequency;
+	var nextArrival = fields.nextTrain;
+	var minAway 	= fields.minTillTrain; 
 
 	// Dynamically creating table rows and table data with the values from the firebase
-	var newRow = $("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + firstTrain + "</td><td>" + minAway + "</td></tr>");
+	var newRow = $("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td>" + minAway + "</td></tr>");
 	// Creating a body variable equal to the train data id so that we can push the newRow data onto the HTML!
 	var body = ($("#train-data"));
 	$(body).append(newRow);
 })
-
-
 
 });
